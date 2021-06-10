f3c85d348655d13407c6c580ee069d5a0df1c16b
pseeth <prem@descript.com>
2021-06-09 18:31:11 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="audiotools",
-    version="0.0.3",
+    version="0.0.4",
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
