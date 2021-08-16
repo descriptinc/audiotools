55eaf500acd8fe8970912f71ae844f6d58012a93
pseeth <prem@descript.com>
2021-08-16 15:05:21 -0700

    Adding magnitude losses to STFT/Mel loss.

--- audiotools/metrics/spectral.py
+++ audiotools/metrics/spectral.py
@@ -17,12 +17,16 @@ class MultiScaleSTFTLoss(nn.Module):
         window_lengths: List[int] = [2048, 512],
         loss_fn=nn.L1Loss(),
         clamp_eps: float = 1e-5,
+        mag_weight: float = 1.0,
+        log_weight: float = 1.0,
     ):
         super().__init__()
         self.stft_params = [
             STFTParams(window_length=w, hop_length=w // 4) for w in window_lengths
         ]
         self.loss_fn = loss_fn
+        self.log_weight = log_weight
+        self.mag_weight = mag_weight
         self.clamp_eps = clamp_eps
 
     def forward(self, x: AudioSignal, y: AudioSignal):
@@ -30,10 +34,11 @@ class MultiScaleSTFTLoss(nn.Module):
         for s in self.stft_params:
             x.stft(s.window_length, s.hop_length, s.window_type)
             y.stft(s.window_length, s.hop_length, s.window_type)
-            loss += self.loss_fn(
+            loss += self.log_weight * self.loss_fn(
                 x.magnitude.pow(2).clamp(self.clamp_eps).log10(),
                 y.magnitude.pow(2).clamp(self.clamp_eps).log10(),
             )
+            loss += self.mag_weight * self.loss_fn(x.magnitude, y.magnitude)
         return loss
 
 
@@ -46,6 +51,8 @@ class MelSpectrogramLoss(nn.Module):
         window_lengths: List[int] = [2048, 512],
         loss_fn=nn.L1Loss(),
         clamp_eps: float = 1e-5,
+        mag_weight: float = 1.0,
+        log_weight: float = 1.0,
     ):
         super().__init__()
         self.stft_params = [
@@ -54,6 +61,8 @@ class MelSpectrogramLoss(nn.Module):
         self.n_mels = n_mels
         self.loss_fn = loss_fn
         self.clamp_eps = clamp_eps
+        self.log_weight = log_weight
+        self.mag_weight = mag_weight
 
     def forward(self, x: AudioSignal, y: AudioSignal):
         loss = 0.0
@@ -63,14 +72,14 @@ class MelSpectrogramLoss(nn.Module):
                 "hop_length": s.hop_length,
                 "window_type": s.window_type,
             }
-            loss += self.loss_fn(
-                x.mel_spectrogram(n_mels, **kwargs)
-                .pow(2)
-                .clamp(self.clamp_eps)
-                .log10(),
-                y.mel_spectrogram(n_mels, **kwargs)
-                .pow(2)
-                .clamp(self.clamp_eps)
-                .log10(),
+            x_mels = x.mel_spectrogram(n_mels, **kwargs)
+            y_mels = y.mel_spectrogram(n_mels, **kwargs)
+
+            loss += self.log_weight * self.loss_fn(
+                x_mels.pow(2).clamp(self.clamp_eps).log10(),
+                y_mels.pow(2).clamp(self.clamp_eps).log10(),
+            )
+            loss += self.mag_weight * self.loss_fn(
+                x_mels, y_mels
             )
         return loss
