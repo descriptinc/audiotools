57479c5aa22587c46e758bde814c6b980dd96bc4
pseeth <prem@descript.com>
2022-03-07 17:04:26 -0800

    Updating version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="audiotools",
-    version="0.2.3",
+    version="0.2.4",
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
