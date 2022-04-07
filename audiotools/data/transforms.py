bcd7fc61f4e97c87169b1c9f7d6b35c7d2c6a0f9
lgestin <lucas@descript.com>
2022-04-07 14:11:13 -0400

    remove unnecessary check

--- audiotools/data/transforms.py
+++ audiotools/data/transforms.py
@@ -676,12 +676,12 @@ class Smoothing(BaseTransform):
 
     def _transform(self, signal, window):
         sscale = signal.audio_data.abs().max(dim=-1, keepdim=True).values
-        if torch.any(sscale == 0.0):
-            sscale[sscale == 0.0] = 1.0
+        sscale[sscale == 0.0] = 1.0
+
         out = signal.convolve(window)
 
         oscale = out.audio_data.abs().max(dim=-1, keepdim=True).values
-        if torch.any(oscale == 0.0):
-            oscale[oscale == 0.0] = 1.0
+        oscale[oscale == 0.0] = 1.0
+
         out = out * (sscale / oscale)
         return out
