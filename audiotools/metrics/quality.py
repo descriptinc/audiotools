9d10d17310dd591eef2cc863bb9fe50c13c6566d
pseeth <prem@descript.com>
2022-10-16 01:17:18 -0700

    Done with metrics.

--- audiotools/metrics/quality.py
+++ audiotools/metrics/quality.py
@@ -10,8 +10,41 @@ def stoi(
     references: AudioSignal,
     extended: int = False,
 ):
-    estimates = estimates.deepcopy().to_mono()
-    references = references.deepcopy().to_mono()
+    """Short term objective intelligibility
+    Computes the STOI (See [1][2]) of a denoised signal compared to a clean
+    signal, The output is expected to have a monotonic relation with the
+    subjective speech-intelligibility, where a higher score denotes better
+    speech intelligibility. Uses pystoi under the hood.
+
+    Parameters
+    ----------
+    estimates : AudioSignal
+        Denoised speech
+    references : AudioSignal
+        Clean original speech
+    extended : int, optional
+        Boolean, whether to use the extended STOI described in [3], by default False
+
+    Returns
+    -------
+    float
+        Short time objective intelligibility measure between clean and
+        denoised speech
+
+    References
+    ----------
+    1.  C.H.Taal, R.C.Hendriks, R.Heusdens, J.Jensen 'A Short-Time
+        Objective Intelligibility Measure for Time-Frequency Weighted Noisy
+        Speech', ICASSP 2010, Texas, Dallas.
+    2.  C.H.Taal, R.C.Hendriks, R.Heusdens, J.Jensen 'An Algorithm for
+        Intelligibility Prediction of Time-Frequency Weighted Noisy Speech',
+        IEEE Transactions on Audio, Speech, and Language Processing, 2011.
+    3.  Jesper Jensen and Cees H. Taal, 'An Algorithm for Predicting the
+        Intelligibility of Speech Masked by Modulated Noise Maskers',
+        IEEE Transactions on Audio, Speech and Language Processing, 2016.
+    """
+    estimates = estimates.clone().to_mono()
+    references = references.clone().to_mono()
 
     stois = []
     for i in range(estimates.batch_size):
@@ -31,10 +64,28 @@ def pesq(
     mode: str = "wb",
     target_sr: float = 16000,
 ):
+    """_summary_
+
+    Parameters
+    ----------
+    estimates : AudioSignal
+        Degraded AudioSignal
+    references : AudioSignal
+        Reference AudioSignal
+    mode : str, optional
+        'wb' (wide-band) or 'nb' (narrow-band), by default "wb"
+    target_sr : float, optional
+        Target sample rate, by default 16000
+
+    Returns
+    -------
+    float
+        PESQ score: P.862.2 Prediction (MOS-LQO)
+    """
     from pesq import pesq as pesq_fn
 
-    estimates = estimates.deepcopy().to_mono().resample(target_sr)
-    references = references.deepcopy().to_mono().resample(target_sr)
+    estimates = estimates.clone().to_mono().resample(target_sr)
+    references = references.clone().to_mono().resample(target_sr)
 
     pesqs = []
     for i in range(estimates.batch_size):
--- audiotools/ml/trainer.py
+++ audiotools/ml/trainer.py
@@ -5,6 +5,7 @@ progress bars from Rich. Requirements:
 import contextlib
 import heapq
 import time
+import typing
 from collections import defaultdict
 from datetime import timedelta
 
@@ -26,7 +27,7 @@ from rich.table import Table
 from torch.utils.tensorboard import SummaryWriter
 
 
-def iter_summary(output, width=None) -> Table:
+def _iter_summary(output, width=None) -> Table:
     """Make a table summarizing each iteration."""
     table = Table(expand=True, width=width)
     table.add_column("Key", style="cyan")
@@ -38,7 +39,7 @@ def iter_summary(output, width=None) -> Table:
     return table
 
 
-def epoch_summary(epoch, output, width=None) -> Table:
+def _epoch_summary(epoch, output, width=None) -> Table:
     """Make a table summarizing each epoch."""
     expand = width is not None
     table = Table(title=f"Summary for Epoch {epoch}", expand=expand, width=width)
@@ -56,6 +57,8 @@ def epoch_summary(epoch, output, width=None) -> Table:
 
 
 class SimpleTimer:
+    """Simple object for timing how long loops take."""
+
     def __init__(self):
         self.start_time = time.time()
 
@@ -67,83 +70,91 @@ class SimpleTimer:
 
 
 class BaseTrainer:
+    """
+    Sub-class this and implement three functions:
+
+    1. train_loop: The training loop.
+    2. val_loop: The validation loop.
+    3. checkpoint: What to checkpoint at the end of each epoch.
+       Use ``self.is_best(engine, metric_you_care_about)`` to figure out
+       if the model should be saved to latest or best. Or use
+       ``self.top_k(engine, metric_you_care_about, k)`` to see if the
+       model you're saving is in the Top K of all models so far.
+
+    Note that if you're in PyCharm, you'll need to do this to
+    get it to work: "PyCharm users will need to enable “emulate terminal”
+    in output console option in run/debug configuration to see
+    styled output." (from Rich docs).
+
+    If you're using this with ``distributed = True``, then only the
+    rank 0 process will do logging. Additionally, self.checkpoint will
+    only run in the rank 0 process.
+
+    Example
+    -------
+    >>> model = ...
+    >>> train_data = ...
+    >>> val_data = ...
+    >>> tb = SummaryWriter(...)
+    >>>
+    >>> class Trainer(BaseTrainer):
+    >>>     def train_loop(self, engine, batch):
+    >>>         batch = util.prepare_batch(batch)
+    >>>         output = model(batch)
+    >>>         sleep(0.01)
+    >>>         return {"l1": np.random.randn(), "mse": np.random.randn()}
+    >>>
+    >>>     def val_loop(self, engine, batch):
+    >>>         batch = util.prepare_batch(batch)
+    >>>         output = model(batch)
+    >>>         sleep(0.01)
+    >>>         return {"l1": np.random.randn(), "mse": np.random.randn()}
+    >>>
+    >>>     def checkpoint(self, engine):
+    >>>         epoch = engine.state.epoch
+    >>>         model.save('checkpoints/latest.model.pth')
+    >>>         if self.is_best(engine, 'mse/val'):
+    >>>             model.save('checkpoints/best.model.pth')
+    >>>         if self.top_k(engine, 'mse/val', 5):
+    >>>             model.save(f'checkpoints/top5.{epoch}.model.pth')
+    >>>
+    >>> trainer = Trainer(writer=tb)
+    >>> trainer.run(train_data, val_data, num_epochs=3)
+
+    Parameters
+    ----------
+    writer : SummaryWriter, optional
+        Writer for Tensorboard. If this is not the rank 0 process, and
+        distributed is False, it will be set to None, by default None.
+    width : int, optional
+        Width of the tables to render, by default 87
+    refresh_rate : int, optional
+        Refresh rate of progress bars, by default 10
+    rank : int, optional
+        The rank of the local process, by default 0.
+    quiet : bool, optional
+        Whether or not to show progress bars during training, by default
+        False.
+    record_memory : bool, optional
+        Whether to profile GPU memory on each loop, by default False.
+    log_file : str, optional
+        Place to write all recorded stdout to, by default ``log.txt``
+    kwargs : dict, optional
+        All additional keyword arguments are saved as attributes of the
+        ``trainer`` (e.g. ``self.optimizer, self.model``).
+    """
+
     def __init__(
         self,
         writer: SummaryWriter = None,
         width: int = 87,
         refresh_rate: float = 1.0,
-        rank=0,
+        rank: int = 0,
         quiet: bool = False,
         record_memory: bool = False,
         log_file: str = "log.txt",
         **kwargs,
     ):
-        """
-        Sub-class this and implement three functions:
-
-        1. train_loop: The training loop.
-        2. val_loop: The validation loop.
-        3. checkpoint: What to checkpoint at the end of each epoch.
-           Use self.is_best(engine, metric_you_care_about) to figure out
-           if the model should be saved to latest or best. Or use
-           self.top_k(engine, metric_you_care_about, k) to see if the
-           model you're saving is in the Top K of all models so far.
-
-        Note that if you're in PyCharm, you'll need to do this to
-        get it to work: "PyCharm users will need to enable “emulate terminal”
-        in output console option in run/debug configuration to see
-        styled output." (from Rich docs).
-
-        If you're using this with distributed = True, then only the
-        rank 0 process will do logging. Additionally, self.val_loop
-        and self.checkpoint will only run in the rank 0 process.
-
-        Example
-        -------
-        >>> model = ...
-        >>> train_data = ...
-        >>> val_data = ...
-        >>> tb = SummaryWriter(...)
-        >>>
-        >>> class Trainer(BaseTrainer):
-        >>>     def train_loop(self, engine, batch):
-        >>>         batch = self.prepare_batch(batch)
-        >>>         output = model(batch)
-        >>>         sleep(0.01)
-        >>>         return {"l1": np.random.randn(), "mse": np.random.randn()}
-        >>>
-        >>>     def val_loop(self, engine, batch):
-        >>>         batch = self.prepare_batch(batch)
-        >>>         output = model(batch)
-        >>>         sleep(0.01)
-        >>>         return {"l1": np.random.randn(), "mse": np.random.randn()}
-        >>>
-        >>>     def checkpoint(self, engine):
-        >>>         epoch = engine.state.epoch
-        >>>         model.save('checkpoints/latest.model.pth')
-        >>>         if self.is_best(engine, 'mse/val'):
-        >>>             model.save('checkpoints/best.model.pth')
-        >>>         if self.top_k(engine, 'mse/val', 5):
-        >>>             model.save(f'checkpoints/top5.{epoch}.model.pth')
-        >>>
-        >>> trainer = Trainer(writer=tb)
-        >>> trainer.run(train_data, val_data, num_epochs=3)
-
-        Parameters
-        ----------
-        writer : SummaryWriter, optional
-            Writer for Tensorboard. If this is not the rank 0 process, and
-            distributed is False, it will be set to None, by default None.
-        width : int, optional
-            Width of the tables to render, by default 87
-        refresh_rate : int, optional
-            Refresh rate of progress bars, by default 10
-        rank : int, optional
-            The rank of the local process, by default 0.
-        quiet : bool, optional
-            Whether or not to show progress bars during training, by default
-            False.
-        """
         self.width = width
         self.refresh_rate = refresh_rate
         self.rank = rank
@@ -296,12 +307,14 @@ class BaseTrainer:
         output = self.wrapper(self.val_loop, engine, batch)
         return output
 
-    def train_loop(self, engine, batch):  # pragma: no cover
+    def train_loop(
+        self, engine: ignite.engine.Engine, batch: typing.Union[dict, list]
+    ):  # pragma: no cover
         """
         Performs a single training step of the model
-        given the batch. Use self.prepare_batch to
+        given the batch. Use ``util.prepare_batch`` to
         move the batch to the desired device, or
-        override it and write your own. self.prepare_batch
+        override it and write your own. ``util.prepare_batch``
         expects that your batches are dictionaries of
         tensors.
 
@@ -310,8 +323,8 @@ class BaseTrainer:
         engine : ignite.engine.Engine
             PyTorch Ignite engine. Access engine.state to find
             out things about the engine's current state (e.g.
-            engine.state.iteration, engine.state.epoch).
-        batch : dict, list
+            ``engine.state.iteration, engine.state.epoch``).
+        batch : typing.Union[dict, list]
             A data batch produced by your dataloader.
 
         Returns
@@ -320,19 +333,21 @@ class BaseTrainer:
             Return a dictionary of metrics (e.g. l1 loss), with
             keys corresponding to the metric name and values
             corresponding to Tensors of size 1 containing the
-            value of that metric. .item() will be called on each
+            value of that metric. ``.item()`` will be called on each
             tensor, and the values will be logged to the live
             progress view as well as Tensorboard if self.writer
             is not None.
         """
         raise NotImplementedError()
 
-    def val_loop(self, engine, batch):  # pragma: no cover
+    def val_loop(
+        self, engine: ignite.engine.Engine, batch: typing.Union[dict, list]
+    ):  # pragma: no cover
         """
         Performs a single validation step of the model
-        given the batch. Use self.prepare_batch to
+        given the batch. Use ``util.prepare_batch`` to
         move the batch to the desired device, or
-        override it and write your own. self.prepare_batch
+        override it and write your own. ``util.prepare_batch``
         expects that your batches are dictionaries of
         tensors.
 
@@ -341,8 +356,8 @@ class BaseTrainer:
         engine : ignite.engine.Engine
             PyTorch Ignite engine. Access engine.state to find
             out things about the engine's current state (e.g.
-            engine.state.iteration, engine.state.epoch).
-        batch : dict, list
+            ``engine.state.iteration, engine.state.epoch``).
+        batch : typing.Union[dict, list]
             A data batch produced by your dataloader.
 
         Returns
@@ -351,18 +366,18 @@ class BaseTrainer:
             Return a dictionary of metrics (e.g. l1 loss), with
             keys corresponding to the metric name and values
             corresponding to Tensors of size 1 containing the
-            value of that metric. .item() will be called on each
+            value of that metric. ``.item()`` will be called on each
             tensor, and the values will be logged to the live
             progress view as well as Tensorboard if self.writer
             is not None.
         """
         raise NotImplementedError()
 
-    def checkpoint(self, engine):  # pragma: no cover
+    def checkpoint(self, engine: ignite.engine.Engine):  # pragma: no cover
         """
-        This callback is called after Events.EPOCH_COMPLETED.
+        This callback is called after ``Events.EPOCH_COMPLETED``.
         You can use it to save models, optimizer, and whatever
-        else you want to do. Use with self.is_best and self.top_k
+        else you want to do. Use with ``self.is_best`` and ``self.top_k``
         to figure out if the model is the best model according to
         some metric (e.g. validation loss), or in the top K models
         so far. You can use these to decide where to save the model
@@ -373,20 +388,20 @@ class BaseTrainer:
         engine : ignite.engine.Engine
             PyTorch Ignite engine. Access engine.state to find
             out things about the engine's current state (e.g.
-            engine.state.iteration, engine.state.epoch).
+            ``engine.state.iteration, engine.state.epoch``).
         """
         pass
 
-    def after_epoch(self, engine):
+    def after_epoch(self, engine: ignite.engine.Engine):
         """
-        This callback is called after Events.EPOCH_COMPLETED, but
+        This callback is called after ``Events.EPOCH_COMPLETED``, but
         in every process (if you are using DDP).
         """
         pass
 
-    def before_epoch(self, engine):
+    def before_epoch(self, engine: ignite.engine.Engine):
         """
-        This callback is called after Events.EPOCH_STARTED, but
+        This callback is called after ``Events.EPOCH_STARTED``, but
         in every process (if you are using DDP). It can allow you to implement
         a curriculum however you see fit - for example check the epoch or
         iteration number, and change the audio duration during training from
@@ -397,24 +412,59 @@ class BaseTrainer:
         engine : ignite.engine.Engine
             PyTorch Ignite engine. Access engine.state to find
             out things about the engine's current state (e.g.
-            engine.state.iteration, engine.state.epoch).
+            ``engine.state.iteration, engine.state.epoch``).
         """
         pass
 
     @staticmethod
-    def is_best(engine, loss_key, minimize=True):
-        """
-        Is this the best model so far, according to loss_key?
+    def is_best(engine: ignite.engine.Engine, loss_key: str, minimize: bool = True):
+        """Determines if the latest model is the best model of all
+        the losses recorded for a specific loss key. Best
+        is either the lowest value (``minimize=True``) or the
+        maximum value (``minimize=False``).
+
+        Parameters
+        ----------
+        engine : ignite.engine.Engine
+            PyTorch Ignite engine.
+        loss_key : str
+            Loss key to look for.
+        minimize : bool, optional
+            Whether to minimize or maximize the loss value, by default True
+
+        Returns
+        -------
+        bool
+            Whether this is the best model so far according to the loss.
         """
         loss_vals = engine.state.logs["epoch"][loss_key]
         fn = min if minimize else max
         return loss_vals[-1] == fn(loss_vals)
 
     @staticmethod
-    def top_k(engine, loss_key, k, minimize=True):
-        """
-        Is this model in the top K models so far, according
-        to loss_key?
+    def top_k(
+        engine: ignite.engine.Engine, loss_key: str, k: int, minimize: bool = True
+    ):
+        """Determines if the latest model is in the top k best of all
+        the losses recorded for a specific loss key. Best
+        is either the lowest value (``minimize=True``) or the
+        maximum value (``minimize=False``).
+
+        Parameters
+        ----------
+        engine : ignite.engine.Engine
+            PyTorch Ignite engine.
+        loss_key : str
+            Loss key to look for.
+        k : int
+            k of top K fame
+        minimize : bool, optional
+            Whether to minimize or maximize the loss value, by default True
+
+        Returns
+        -------
+        bool
+            Whether this is in the top k models so far according to the loss.
         """
         loss_vals = engine.state.logs["epoch"][loss_key]
         fn = heapq.nsmallest if minimize else heapq.nlargest
@@ -427,11 +477,22 @@ class BaseTrainer:
         return model_rank
 
     def print(self, *args, **kwargs):
+        """Prints to both standard out, as well as the log file, while not
+        disturbing the live view produced by ``rich``.
+        """
         if self.rank == 0:
             self.live.console.print(*args, **kwargs)
             self.file_console.print(*args, **kwargs)
 
-    def validate(self, engine):
+    def validate(self, engine: ignite.engine.Engine):
+        """Runs the validator engine over the validation
+        data.
+
+        Parameters
+        ----------
+        engine : ignite.engine.Engine
+            PyTorch Ignite engine.
+        """
         if self.val_data is not None:
             self.validator.run(self.val_data)
 
@@ -464,7 +525,7 @@ class BaseTrainer:
         summary["Epoch took"] = str(timedelta(seconds=self.epoch_timer.value()))
         summary["Time since start"] = str(timedelta(seconds=self.overall_timer.value()))
 
-        self.epoch_summary = epoch_summary(engine.state.epoch, summary, self.width)
+        self.epoch_summary = _epoch_summary(engine.state.epoch, summary, self.width)
         self.print(self.epoch_summary)
 
         if not self.quiet:
@@ -516,7 +577,7 @@ class BaseTrainer:
         if self.quiet:
             return
 
-        iter_table = iter_summary(engine.state.output, self.width)
+        iter_table = _iter_summary(engine.state.output, self.width)
         self.pbar.advance(engine.state.pbar)
 
         updated_table = Table.grid()
