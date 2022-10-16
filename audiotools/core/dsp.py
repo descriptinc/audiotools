3f86ed13ef62c0750f34c197b034490776ee9d32
pseeth <prem@descript.com>
2022-10-15 20:45:32 -0700

    Finished DSP.

--- audiotools/core/dsp.py
+++ audiotools/core/dsp.py
@@ -1,7 +1,8 @@
+import typing
+
 import julius
 import numpy as np
 import torch
-import torchaudio
 
 from . import util
 
@@ -27,7 +28,27 @@ class DSPMixin:
 
         return window_length, hop_length
 
-    def windows(self, window_duration, hop_duration, preprocess: bool = True):
+    def windows(
+        self, window_duration: float, hop_duration: float, preprocess: bool = True
+    ):
+        """Generator which yields windows of specified duration from signal with a specified
+        hop length.
+
+        Parameters
+        ----------
+        window_duration : float
+            Duration of every window in seconds.
+        hop_duration : float
+            Hop between windows in seconds.
+        preprocess : bool, optional
+            Whether to preprocess the signal, so that the first sample is in
+            the middle of the first window, by default True
+
+        Yields
+        ------
+        AudioSignal
+            Each window is returned as an AudioSignal.
+        """
         if preprocess:
             window_length, hop_length = self._preprocess_signal_for_windowing(
                 window_duration, hop_duration
@@ -46,18 +67,28 @@ class DSPMixin:
                     break
                 yield self[b, ..., start_idx:end_idx]
 
-    def collect_windows(self, window_duration, hop_duration, preprocess: bool = True):
-        """Function which collects overlapping windows from
-        an AudioSignal.
+    def collect_windows(
+        self, window_duration: float, hop_duration: float, preprocess: bool = True
+    ):
+        """Reshapes signal into windows of specified duration from signal with a specified
+        hop length. Window are placed along the batch dimension. Use with
+        :py:func:`audiotools.core.dsp.DSPMixin.overlap_and_add` to reconstruct the
+        original signal.
 
-        Args:
-            audio_signal (AudioSignal): AudioSignal that windows will be collected over.
-            window_duration (float): Length of window in seconds.
-            hop_duration (float): How much to shift for each window
-                (overlap is window_duration - hop_duration) in seconds.
+        Parameters
+        ----------
+        window_duration : float
+            Duration of every window in seconds.
+        hop_duration : float
+            Hop between windows in seconds.
+        preprocess : bool, optional
+            Whether to preprocess the signal, so that the first sample is in
+            the middle of the first window, by default True
 
-        Returns:
-            AudioSignal: Signal of shape (nb * num_windows, nc, window_length).
+        Returns
+        -------
+        AudioSignal
+            AudioSignal unfolded with shape ``(nb * nch * num_windows, 1, window_length)``
         """
         if preprocess:
             window_length, hop_length = self._preprocess_signal_for_windowing(
@@ -76,20 +107,20 @@ class DSPMixin:
         self.audio_data = unfolded
         return self
 
-    def overlap_and_add(self, hop_duration):
+    def overlap_and_add(self, hop_duration: float):
         """Function which takes a list of windows and overlap adds them into a
-        signal the same length as `audio_signal`.
+        signal the same length as ``audio_signal``.
 
-        Args:
-            windows (list): List of audio signal objects containing each window, produced by
-                `OverlapAdd.collect_windows`.
-            sample_rate (float): Sample rate of audio signal.
-            window_duration (float): Length of window in seconds.
-            hop_duration (float): How much to shift for each window
-                (overlap is window_duration - hop_duration) in seconds.
+        Parameters
+        ----------
+        hop_duration : float
+            How much to shift for each window
+            (overlap is window_duration - hop_duration) in seconds.
 
-        Returns:
-            AudioSignal: overlap-and-added signal.
+        Returns
+        -------
+        AudioSignal
+            overlap-and-added signal.
         """
         hop_length = int(hop_duration * self.sample_rate)
         window_length = self.signal_length
@@ -119,7 +150,26 @@ class DSPMixin:
         self.trim(hop_length, hop_length)
         return self
 
-    def low_pass(self, cutoffs, zeros=51):
+    def low_pass(
+        self, cutoffs: typing.Union[torch.Tensor, np.ndarray, float], zeros: int = 51
+    ):
+        """Low-passes the signal in-place. Each item in the batch
+        can have a different low-pass cutoff, if the input
+        to this signal is an array or tensor. If a float, all
+        items are given the same low-pass filter.
+
+        Parameters
+        ----------
+        cutoffs : typing.Union[torch.Tensor, np.ndarray, float]
+            Cutoff in Hz of low-pass filter.
+        zeros : int, optional
+            Number of taps to use in low-pass filter, by default 51
+
+        Returns
+        -------
+        AudioSignal
+            Low-passed AudioSignal.
+        """
         cutoffs = util.ensure_tensor(cutoffs, 2, self.batch_size)
         cutoffs = cutoffs / self.sample_rate
         filtered = torch.empty_like(self.audio_data)
@@ -132,7 +182,26 @@ class DSPMixin:
         self.stft_data = None
         return self
 
-    def high_pass(self, cutoffs, zeros=51):
+    def high_pass(
+        self, cutoffs: typing.Union[torch.Tensor, np.ndarray, float], zeros: int = 51
+    ):
+        """High-passes the signal in-place. Each item in the batch
+        can have a different high-pass cutoff, if the input
+        to this signal is an array or tensor. If a float, all
+        items are given the same high-pass filter.
+
+        Parameters
+        ----------
+        cutoffs : typing.Union[torch.Tensor, np.ndarray, float]
+            Cutoff in Hz of high-pass filter.
+        zeros : int, optional
+            Number of taps to use in high-pass filter, by default 51
+
+        Returns
+        -------
+        AudioSignal
+            High-passed AudioSignal.
+        """
         cutoffs = util.ensure_tensor(cutoffs, 2, self.batch_size)
         cutoffs = cutoffs / self.sample_rate
         filtered = torch.empty_like(self.audio_data)
@@ -145,7 +214,31 @@ class DSPMixin:
         self.stft_data = None
         return self
 
-    def mask_frequencies(self, fmin_hz: int, fmax_hz: int, val: float = 0.0):
+    def mask_frequencies(
+        self,
+        fmin_hz: typing.Union[torch.Tensor, np.ndarray, float],
+        fmax_hz: typing.Union[torch.Tensor, np.ndarray, float],
+        val: float = 0.0,
+    ):
+        """Masks frequencies between ``fmin_hz`` and ``fmax_hz``, and fills them
+        with the value specified by ``val``. Useful for implementing SpecAug.
+        The min and max can be different for every item in the batch.
+
+        Parameters
+        ----------
+        fmin_hz : typing.Union[torch.Tensor, np.ndarray, float]
+            Lower end of band to mask out.
+        fmax_hz : typing.Union[torch.Tensor, np.ndarray, float]
+            Upper end of band to mask out.
+        val : float, optional
+            Value to fill in, by default 0.0
+
+        Returns
+        -------
+        AudioSignal
+            Signal with ``stft_data`` manipulated. Apply ``.istft()`` to get the
+            masked audio data.
+        """
         # SpecAug
         mag, phase = self.magnitude, self.phase
         fmin_hz = util.ensure_tensor(fmin_hz, ndim=mag.ndim)
@@ -166,7 +259,31 @@ class DSPMixin:
         self.stft_data = mag * torch.exp(1j * phase)
         return self
 
-    def mask_timesteps(self, tmin_s: int, tmax_s: int, val: float = 0.0):
+    def mask_timesteps(
+        self,
+        tmin_s: typing.Union[torch.Tensor, np.ndarray, float],
+        tmax_s: typing.Union[torch.Tensor, np.ndarray, float],
+        val: float = 0.0,
+    ):
+        """Masks teimsteps between ``tmin_s`` and ``tmax_s``, and fills them
+        with the value specified by ``val``. Useful for implementing SpecAug.
+        The min and max can be different for every item in the batch.
+
+        Parameters
+        ----------
+        tmin_s : typing.Union[torch.Tensor, np.ndarray, float]
+            Lower end of timesteps to mask out.
+        tmax_s : typing.Union[torch.Tensor, np.ndarray, float]
+            Upper end of timesteps to mask out.
+        val : float, optional
+            Value to fill in, by default 0.0
+
+        Returns
+        -------
+        AudioSignal
+            Signal with ``stft_data`` manipulated. Apply ``.istft()`` to get the
+            masked audio data.
+        """
         # SpecAug
         mag, phase = self.magnitude, self.phase
         tmin_s = util.ensure_tensor(tmin_s, ndim=mag.ndim)
@@ -187,7 +304,25 @@ class DSPMixin:
         self.stft_data = mag * torch.exp(1j * phase)
         return self
 
-    def mask_low_magnitudes(self, db_cutoff: float, val: float = 0.0):
+    def mask_low_magnitudes(
+        self, db_cutoff: typing.Union[torch.Tensor, np.ndarray, float], val: float = 0.0
+    ):
+        """Mask away magnitudes below a specified threshold, which
+        can be different for every item in the batch.
+
+        Parameters
+        ----------
+        db_cutoff : typing.Union[torch.Tensor, np.ndarray, float]
+            Decibel value for which things below it will be masked away.
+        val : float, optional
+            Value to fill in for masked portions, by default 0.0
+
+        Returns
+        -------
+        AudioSignal
+            Signal with ``stft_data`` manipulated. Apply ``.istft()`` to get the
+            masked audio data.
+        """
         mag = self.magnitude
         log_mag = self.log_magnitude()
 
@@ -198,17 +333,56 @@ class DSPMixin:
         self.magnitude = mag
         return self
 
-    def shift_phase(self, shift: float):
+    def shift_phase(self, shift: typing.Union[torch.Tensor, np.ndarray, float]):
+        """Shifts the phase by a constant value.
+
+        Parameters
+        ----------
+        shift : typing.Union[torch.Tensor, np.ndarray, float]
+            What to shift the phase by.
+
+        Returns
+        -------
+        AudioSignal
+            Signal with ``stft_data`` manipulated. Apply ``.istft()`` to get the
+            masked audio data.
+        """
         shift = util.ensure_tensor(shift, ndim=self.phase.ndim)
         self.phase = self.phase + shift
         return self
 
-    def corrupt_phase(self, scale: float):
+    def corrupt_phase(self, scale: typing.Union[torch.Tensor, np.ndarray, float]):
+        """Corrupts the phase randomly by some scaled value.
+
+        Parameters
+        ----------
+        scale : typing.Union[torch.Tensor, np.ndarray, float]
+            Standard deviation of noise to add to the phase.
+
+        Returns
+        -------
+        AudioSignal
+            Signal with ``stft_data`` manipulated. Apply ``.istft()`` to get the
+            masked audio data.
+        """
         scale = util.ensure_tensor(scale, ndim=self.phase.ndim)
         self.phase = self.phase + scale * torch.randn_like(self.phase)
         return self
 
     def preemphasis(self, coef: float = 0.85):
+        """Applies pre-emphasis to audio signal.
+
+        Parameters
+        ----------
+        coef : float, optional
+            How much pre-emphasis to apply, lower values do less. 0 does nothing.
+            by default 0.85
+
+        Returns
+        -------
+        AudioSignal
+            Pre-emphasized signal.
+        """
         kernel = torch.tensor([1, -coef, 0]).view(1, 1, -1).to(self.device)
         x = self.audio_data.reshape(-1, 1, self.signal_length)
         x = torch.nn.functional.conv1d(x, kernel, padding=1)
