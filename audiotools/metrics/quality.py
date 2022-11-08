87dd198289b65ca5e5b1b6c3c44796e9670ba2e0
Alejandro Luebs <alejandro@descript.com>
2022-11-08 17:20:58 -0500

    Run linter on ViSQOL changes

--- audiotools/metrics/quality.py
+++ audiotools/metrics/quality.py
@@ -1,5 +1,6 @@
-import numpy as np
 import os
+
+import numpy as np
 import torch
 
 from .. import AudioSignal
@@ -104,7 +105,7 @@ def pesq(
 def visqol(
     estimates: AudioSignal,
     references: AudioSignal,
-    mode: str = 'audio',
+    mode: str = "audio",
 ):
     """ViSQOL score.
 
@@ -126,22 +127,23 @@ def visqol(
     import visqol_config_pb2
     import similarity_result_pb2
 
-
     config = visqol_config_pb2.VisqolConfig()
-    if mode == 'audio':
+    if mode == "audio":
         target_sr = 48000
         config.options.use_speech_scoring = False
-        svr_model_path = 'libsvm_nu_svr_model.txt'
-    elif mode == 'speech':
+        svr_model_path = "libsvm_nu_svr_model.txt"
+    elif mode == "speech":
         target_sr = 16000
         config.options.use_speech_scoring = True
-        svr_model_path = 'lattice_tcditugenmeetpackhref_ls2_nl60_lr12_bs2048_learn.005_ep2400_train1_7_raw.tflite'
+        svr_model_path = "lattice_tcditugenmeetpackhref_ls2_nl60_lr12_bs2048_learn.005_ep2400_train1_7_raw.tflite"
     else:
         raise ValueError(f"Unrecognized mode: {mode}")
     config.audio.sample_rate = target_sr
     config.options.svr_model_path = os.path.join(
-            os.path.dirname(visqol_config_pb2.__file__),
-            'visqol.runfiles/__main__/model', svr_model_path)
+        os.path.dirname(visqol_config_pb2.__file__),
+        "visqol.runfiles/__main__/model",
+        svr_model_path,
+    )
 
     api = visqol_lib_py.VisqolApi()
     api.Create(config)
@@ -153,7 +155,7 @@ def visqol(
     for i in range(estimates.batch_size):
         _visqol = api.Measure(
             references.audio_data[i, 0].detach().cpu().numpy().astype(np.float),
-            estimates.audio_data[i, 0].detach().cpu().numpy().astype(np.float)
+            estimates.audio_data[i, 0].detach().cpu().numpy().astype(np.float),
         )
         visqols.append(_visqol.moslqo)
     return torch.from_numpy(np.array(visqols))
