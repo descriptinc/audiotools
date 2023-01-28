22153662c0ade5eb7d9d734755dbf88ae8b32df6
pseeth <prem@descript.com>
2023-01-28 14:43:08 -0800

    Bigger version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="audiotools",
-    version="0.5.6",
+    version="0.6.0",
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
