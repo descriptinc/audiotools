0eb7f38cee7b8a07a04ceb736678348217f11cf5
pseeth <prem@descript.com>
2023-02-02 20:30:29 -0800

    Bumping version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="audiotools",
-    version="0.6.0",
+    version="0.6.1",
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
