819931930dd98e2f94fdfd82278b25e51e13d096
Ajay Arasanipalai <ajayuppili@descript.com>
2023-05-15 22:30:06 +0000

    style: lazy imports, in-place ops, docstrings

--- audiotools/core/whisper.py
+++ audiotools/core/whisper.py
@@ -1,8 +1,3 @@
-import torch
-from transformers import WhisperForConditionalGeneration
-from transformers import WhisperProcessor
-
-
 class WhisperMixin:
     is_initialized = False
 
@@ -11,6 +6,11 @@ class WhisperMixin:
         pretrained_model_name_or_path: str = "openai/whisper-base.en",
         device: str = "cuda",
     ):
+        from transformers import WhisperForConditionalGeneration
+        from transformers import WhisperProcessor
+        from transformers import PreTrainedTokenizer
+        from transformers import PreTrainedModel
+
         self.whisper_device = device
         self.whisper_processor = WhisperProcessor.from_pretrained(
             pretrained_model_name_or_path
@@ -21,16 +21,27 @@ class WhisperMixin:
         self.is_initialized = True
 
     def get_whisper_features(self):
+        """Preprocess audio signal as per the whisper model's training config.
+
+        Returns
+        -------
+        torch.Tensor
+            The prepinput features of the audio signal. Shape: (1, channels, seq_len)
+        """
+        import torch
+
         if not self.is_initialized:
             self.setup_whisper()
 
         signal = self.to(self.device)
-        raw_speech = [
-            s[0]
-            for s in signal.resample(
-                self.whisper_processor.feature_extractor.sampling_rate
-            ).numpy()
-        ]
+        raw_speech = list(
+            (
+                signal.clone()
+                .resample(self.whisper_processor.feature_extractor.sampling_rate)
+                .audio_data[:, 0, :]
+                .numpy()
+            )
+        )
 
         with torch.inference_mode():
             input_features = self.whisper_processor(
@@ -42,8 +53,17 @@ class WhisperMixin:
         return input_features
 
     def get_whisper_transcript(self) -> str:
+        """Get the transcript of the audio signal using the whisper model.
+
+        Returns
+        -------
+        str
+            The transcript of the audio signal, including special tokens such as <|startoftranscript|> and <|endoftext|>.
+        """
+        import torch
+
         if not self.is_initialized:
-            self.setup()
+            self.setup_whisper()
 
         input_features = self.get_whisper_features()
 
@@ -52,11 +72,20 @@ class WhisperMixin:
             generated_ids = self.whisper_model.generate(inputs=input_features)
 
         transcription = self.whisper_processor.batch_decode(generated_ids)
-        return transcription
+        return transcription[0]
 
-    def get_whisper_embeddings(self) -> torch.Tensor:
+    def get_whisper_embeddings(self):
+        """Get the last hidden state embeddings of the audio signal using the whisper model.
+
+        Returns
+        -------
+        torch.Tensor
+            The Whisper embeddings of the audio signal. Shape: (1, seq_len, hidden_size)
+        """
+        import torch
+
         if not self.is_initialized:
-            self.setup()
+            self.setup_whisper()
 
         input_features = self.get_whisper_features()
         encoder = self.whisper_model.get_encoder()
