455839756a8005c47f5f9ded72fa52acb16f95fc
pseeth <prem@descript.com>
2023-05-26 12:54:37 -0700

    Deleting outdated tools for release.

--- audiotools/ml/__init__.py
+++ audiotools/ml/__init__.py
@@ -1,7 +1,5 @@
 from . import decorators
 from . import layers
-from . import tricks
 from .accelerator import Accelerator
 from .experiment import Experiment
 from .layers import BaseModel
-from .trainer import BaseTrainer
