acb236b9fa6cf8ed8c427318720d0dbd517cb295
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-07-07 12:11:13 +0200

    fixed temporary file not being removed

--- audiotools/core/ffmpeg.py
+++ audiotools/core/ffmpeg.py
@@ -136,6 +136,7 @@ class FFMPEGMixin:
                 command += " -hide_banner -loglevel error"
             subprocess.check_call(shlex.split(command))
             resampled = AudioSignal(f_out)
+            Path.unlink(Path(f.out))
         return resampled
 
     @classmethod
