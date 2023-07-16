726c0779233dc0f037b4fa288b49450014c192f6
prem <prem@descript.com>
2023-07-16 00:32:24 +0000

    Bumping version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="descript-audiotools",
-    version="0.7.2",
+    version="0.7.3",
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
