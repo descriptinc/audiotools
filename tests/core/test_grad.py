b73604e3de586eb829f7ba83ca29f295ef8ed418
Angela Gong <angela@descript.com>
2024-02-01 10:15:00 -0800

    fix test

--- tests/core/test_grad.py
+++ tests/core/test_grad.py
@@ -28,12 +28,12 @@ def test_audio_grad():
                 if result.stft_data is not None:
                     result.istft()
                 if result.audio_data.dtype.is_complex:
-                    result.real().sum().backward()
+                    result.audio_data.real().sum().backward()
                 else:
                     result.audio_data.sum().backward()
             else:
                 if result.dtype.is_complex:
-                    result.real.sum().backward()
+                    result.real().sum().backward()
                 else:
                     result.sum().backward()
 
