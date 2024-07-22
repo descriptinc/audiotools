08a9cf54dc6a335d1369335ff4d3413bdf55da40
Carl Thomé <carl.thome@epidemicsound.com>
2024-07-22 16:34:14 +0200

    Update protobuf version

--- setup.py
+++ setup.py
@@ -53,9 +53,8 @@ setup(
         "flatten-dict",
         "markdown2",
         "randomname",
-        # Have to freeze protobuf version, https://github.com/protocolbuffers/protobuf/issues/10051
-        # Borrowing pin from tensorboard source: https://github.com/tensorflow/tensorboard/commit/fd4f5ff79374252e313c2e7e9b247bc49ab0d54d.
-        "protobuf >= 3.9.2, < 3.20",
+        # Borrowing pin from tensorboard source: https://github.com/tensorflow/tensorboard/commit/e795312f70537cb9c9ab4bd2a08a03e77f0356a5.
+        "protobuf >= 3.19.6, != 4.24.0, < 5.0.0",
         "tensorboard",
         "tqdm",
     ],
